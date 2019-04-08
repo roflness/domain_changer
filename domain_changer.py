48f5227612f949269873ed3a27f0587bba728951
Rafael Reyes <rcreyes.951@gmail.com>
2019-04-07 22:53:35 -0700

    Update domain_changer.py
    Sorts percentage.csv file in descending order (highest first)

--- domain_changer.py
+++ domain_changer.py
@@ -6,9 +6,11 @@ import pandas as pd
 import numpy as np
 import re
 
-df = pd.read_csv(sys.argv[1])
+df = pd.read_csv(sys.argv[1]+'.csv')
 
 df = df.drop_duplicates(keep='first')
+
+df = df[df["email"].str.contains('\.') == True]
 
 df['email'] = df.email.replace(r"^(?:http)s?://", "", regex=True) # Remove http/s from start of string
 df['email'] = df.email.replace(r"^(?:www)\.?", "", regex=True) # Remove www. from start of string
@@ -18,22 +20,25 @@ df['email'] = df.email.replace(r"(/).*", "", regex=True) # Remove everything aft
 
 def domain_it(df):
   return re.split('@', df)[-1]
-  
+
 def domain_type(df):
   return re.split('\.', df)[-1]
-  
+
 func = lambda x: 100*x.count()/df.shape[0] # Used for calculating percentages for count2
-  
+
 # df['domain'] = df['email'].apply(domain_it)
 
 df['domain_type'] = df['email'].apply(domain_type)
 
 count = df.pivot_table(index=['domain_type'], aggfunc='count', margins=True)
-count2 = pd.pivot_table(df, index=['domain_type'], aggfunc=func)
+count2 = pd.pivot_table(df, index=['domain_type'], aggfunc=func).sort_values(by=['email'], ascending=False)
 
 print(count)
 print(count2)
-df.columns = ["email", "domain"]
+# df.columns = ["email", "domain"]
 # print(df)
 
 df.to_csv('output.csv', index=False, columns=["email"], header =True)
+
+count2.to_csv('percentage.csv', index=True, columns=["email"], header=["percentage"])
+# print(new_df)
